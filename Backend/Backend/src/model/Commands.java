37576b7b27b694ca3fd5b2161d3427a3eb04d5a8
idanlale <idanlale1@gmail.com>
2022-06-11 15:48:07 +0300

    Add PrintCommand

--- Backend/Backend/src/model/Commands.java
+++ Backend/Backend/src/model/Commands.java
@@ -1,29 +1,52 @@
 package model;
 
+import java.io.PrintStream;
+
 public class Commands {
+    PrintStream out = System.out;
+
     public interface ICommand {
-        void execute();
+        int execute(String[] arr, int index);
     }
 
     public abstract class Command implements ICommand {
-        public abstract void execute();
+        public abstract int execute(String[] arr, int index);
     }
 
     public class OpenServerCommand extends Command {
-        public void execute() {
+        public int execute(String[] arr, int index) {
             System.out.println("server opened!");
+            return index + 1;
         }
     }
 
     public class ConnectCommand extends Command {
-        public void execute() {
+        public int execute(String[] arr, int index) {
             System.out.println("connected!");
+            return index + 1;
         }
     }
 
     public class DefineVarCommand extends Command {
-        public void execute() {
+        public int execute(String[] arr, int index) {
             System.out.println("var defined!");
+            return index + 1;
+        }
+    }
+
+    public class PrintCommand extends Command {
+        public int execute(String[] arr, int index) {
+            index++;
+            int flag = 0;
+            while (flag != 2 && index < arr.length) {
+                if(arr[index].contains("\"")){
+                    flag++;
+                }
+                arr[index] = arr[index].replace("\"", "");
+                out.println(arr[index]);
+                index++;
+            }
+            return index + 1;
         }
     }
 
--- Backend/Backend/src/model/Interpreter.java
+++ Backend/Backend/src/model/Interpreter.java
@@ -8,9 +8,29 @@ public class Interpreter {
 
     public Interpreter() {
         Commands commands = new Commands();
-        map.put("openServer", commands.new OpenServerCommand());
+        map.put("openDataServer", commands.new OpenServerCommand());
         map.put("connect", commands.new ConnectCommand());
         map.put("define", commands.new DefineVarCommand());
-
+        map.put("print", commands.new PrintCommand());
     }
+
+    public void interpret(String commands) {
+        String[] arr = commands.split(" ");
+        int index = 0;
+        while (index < arr.length) {
+            String command = arr[index];
+            Command c = map.get(command);
+            if (c != null) {
+                index = c.execute(arr, index);
+            } else {
+                index++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Interpreter interpreter = new Interpreter();
+        interpreter.interpret("connect print \"Hello World\"");
+    }
+
 }
